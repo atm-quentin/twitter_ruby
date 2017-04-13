1319922eeaa141ed2f5d79bc692f84c8fc798200
Quentin Vial-Gouteyron <vialgouq@pc-b123-09.iut-valence.fr>
2017-04-13 09:29:43 +0200

    debut follower

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   validates :nom, uniqueness: true
-	has_many :tweets
+	has_many :tweets, dependent: :destroy
+	has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   root               to: 'sessions#new'
-  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :users do
     resources :tweets
   end
